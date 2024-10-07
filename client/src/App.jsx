50b51e63850e80f8df624fbba2acd4fb12f8afdd
ammarahmad1 <ammarahmed266270@gmail.com>
2024-10-07 14:40:20 +0500

    Creating footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Header from './components/Header'
 import { Button } from "flowbite-react";
+import Footer from './components/Footer'
 
 
 export default function App() {
@@ -23,6 +24,7 @@ export default function App() {
         <Route path='/dashboard' element={ <Dashboard /> } />
         <Route path='/projects' element={ <Projects /> } />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
